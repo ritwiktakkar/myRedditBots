bd512bf3556f2c09c9d7507ba1151bdbd28fe49c
Ritwik Takkar <rtakkar@syr.edu>
2019-11-17 15:20:25 -0500

    increase code efficiency and readability

--- theCensoringBot.py
+++ theCensoringBot.py
@@ -1,28 +1,18 @@
 import praw
-import configForTCB   # credentials for this bot -- file not visible on GitHub
 import time 
 
-config = configForTCB
-
-# making the list of profanities' scope global 
+# profanities in txt file made global
 with open('profanity_list.txt', 'r') as f:
-	profanity_list = [line.strip() for line in f]
+		profanity_list = [line.strip() for line in f]
 
-# this method logs the bot in 
-def bot_login():
-	print("Logging in...")
-	
-	r = praw.Reddit(
-		username = config.username,
-		password = config.password,
-		client_id = config.client_id,
-		client_secret = config.client_secret,
-		user_agent = "Im a bot that detects swear words and returns a censored version of the comment")
-	
-	print("Logged in!")
-	return r
 
-def splitSpecialChars(word):
+def authenticate():
+	# logs the bot in 
+	reddit = praw.Reddit('theCensoringBot', user_agent = "I detect swear words and return a censored version of the comment")
+	return reddit
+
+
+def split_special_chars(word):
 	# checks word
 	# if word starts/ends with special char and 
 		# 1) is profanity: returns profanity without special chars
@@ -36,66 +26,94 @@ def splitSpecialChars(word):
 	else:
 		return orig_word
 
-def findAndReplace(word):
+
+def find_and_replace(word):
+	# check word to see if profanity and replace if so
 	if word.lower() in profanity_list:
 		word = '(profanity)'
 	return word
 
+
 def get_saved_comments():
+	# opens list containing comment IDs already replied to so I don't reply again
 	with open("comments_replied_to.txt", "r") as f:
 		comments_replied_to = f.read()
 		comments_replied_to = comments_replied_to.split('\n')
 		comments_replied_to = list(filter(None, comments_replied_to))
 	return comments_replied_to
 
+
 def sleep(seconds):
-	print('Sleeping for ' + str(seconds) + 'seconds')
+	# sleep for some time (seconds) 
+	print('Sleeping for ' + str(seconds) + ' seconds...\n')
 	time.sleep(seconds)
 
-# function that retrieves the comment that summoned the bot
-def censor_comment(r, comments_replied_to):
-	for comments in r.subreddit('botTesting123456').comments():
-		if comments.id in comments_replied_to:
-				break
-		else:
-			parentname = comments.parent().author
-			childname = comments.author
-			if 'censor-this!' in comments.body and childname != r.user.me() and parentname != r.user.me():
-				parentcomment = comments.parent().body
-				childcomment = comments.body
-				wordsInUC_unrev = parentcomment.split() # puts each word in UC into a list
-				wordsInUC_unrev2 = []
-				wordsInCC = []
-				for words in wordsInUC_unrev:
-					words = splitSpecialChars(words)
-					wordsInUC_unrev2.append(words)
-				for words in wordsInUC_unrev2:
-					words = findAndReplace(words)
-					wordsInCC.append(words)
-				count = 0
-				for words in wordsInCC:
-					if words == '(profanity)':
-						count += 1
-				censoredComment = ' '.join(wordsInCC)
 
-				comments.reply('I am a bot, *bleep*, *bloop*. I found ' + str(count) + ' swear word(s) in /u/' 
-								+ str(parentname) + '\'s comment.\n\n' + '**Here is a censored version of their comment:**' 
-				                + '\n\n________________________________________________\n\n' 
-				                + censoredComment
-				                + '\n\n________________________________________________\n\n' 
-								+ 'Go [here](https://www.reddit.com/user/theCensoringBot/comments/dwssjj/about_me/) to learn more about me: theCensoringBot'
-								)
+def censor_comment(reddit, comments_replied_to):
+	# read specific comments in particular subreddit based on some attribute
+	# if comment contains keyword, work my magic and reply with the censored comment
+	# add comment id of comment I replied to to my list so I don't reply again
+	# sleep for some time
+	for submissions in reddit.subreddit('botTesting123456').rising(limit = 3): 
+		for comments in submissions.comments.list():
+			
+			if comments.id not in comments_replied_to:
+				parent_name = comments.parent().author
+				child_name = comments.author
+				if 'censor-this!' in comments.body and child_name != reddit.user.me() and parent_name != reddit.user.me():
+					print('new request found!\n')
+					parent_comment = comments.parent().body
+					child_comment = comments.body
+					try:
+						temp = parent_comment.split() # puts each word in UC into a list
+						words_in_uc = []
+						words_in_cc = []
+						
+						for words in temp:
+							words = split_special_chars(words)
+							words_in_uc.append(words)
+						
+						for words in words_in_uc:
+							words = find_and_replace(words)
+							words_in_cc.append(words)
+						
+						count = 0
+						for words in words_in_cc:
+							if words == '(profanity)':
+								count += 1
+						
+						censored_comment = ' '.join(words_in_cc)
 
-				comments_replied_to.append(comments.id)
-				with open("comments_replied_to.txt", "a") as f:
-					f.write(comments.id + '\n')
+						comments.reply('I am a bot, *bleep*, *bloop*. I found ' + str(count) + ' swear word(s) in /u/' 
+										+ str(parent_name) + '\'s comment.\n\n' + '**Here is a censored version of their comment:**' 
+						                + '\n\n________________________________________________\n\n' 
+						                + censored_comment
+						                + '\n\n________________________________________________\n\n' 
+										+ 'Go [here](https://www.reddit.com/user/theCensoringBot/comments/dwssjj/about_me/) to learn more about me\n\n' 
+										)
+						
+						comments_replied_to.append(comments.id)
+						
+						with open("comments_replied_to.txt", "a") as f:
+							f.write(comments.id + '\n')
 
-				sleep(4)
+					except AttributeError:
+						print('captured AttributeError')
+						comments_replied_to.append(comments.id)
+						with open("comments_replied_to.txt", "a") as f:
+							f.write(comments.id + '\n')
+						
+					sleep(5)
 
 
-r = bot_login()
+def main(): 
+	# run the bot
+	reddit = authenticate()
+	print('Authenticated as /u/{}\n'.format(reddit.user.me()))
+	comments_replied_to = get_saved_comments()
+	while True:
+		censor_comment(reddit, comments_replied_to)
 
-comments_replied_to = get_saved_comments()
 
-while True:
-	censor_comment(r, comments_replied_to)
+if __name__ == '__main__':
+	main()
